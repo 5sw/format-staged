98302d88ea9e3243220e3e3ce7516036d3eb5fce
Sven Weidauer <sven@5sw.de>
2022-05-29 09:54:43 +0200

    Single quotes

--- lib/format_staged.rb
+++ lib/format_staged.rb
@@ -105,7 +105,7 @@ class FormatStaged
   end
 
   def patch_working_file(file, new_hash)
-    info "Updating working copy"
+    info 'Updating working copy'
 
     patch = get_output 'git', 'diff', file.dst_hash, new_hash, lines: false, silent: true
     patch.gsub! "a/#{file.dst_hash}", "a/#{file.src_path}"
